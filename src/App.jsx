cf6cbdca64b30bce15cee7d12aaacd9fd543ad5f
nittish03 <nittishbaboria03@gmail.com>
2025-01-07 14:14:44 +0530

    minor change

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import './App.css'
 import { Routes, Route } from 'react-router-dom'
 import Qrcode from './components/Qrcode'
 import Navbar from './components/Navbar'
-import ImageGenerator from './components/ImageGenerator'
+
 
 
 function App() {
