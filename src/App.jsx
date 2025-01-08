57c64abf7ab555b4925bb81fd8d6265e56af9f2d
KOSANG YADAV <kosangyadav2004@gmail.com>
2025-01-08 19:19:46 +0530

    delete button and qrcode

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import { useState } from "react";
 import "./App.css";
 import {UploadPage, Home, Profile, Dashboard} from "./index.js"
 import { Routes, Route } from "react-router-dom";
-import Qrcode from "./components/Qrcode";
 import Navbar from "./components/Navbar";
 import { Toaster } from 'react-hot-toast';
 
--- src/index.js
+++ src/index.js
@@ -1,9 +1,8 @@
-//import components
-import Button from "./components/Button"
+
 
 import UploadPage from "./pages/Upload";
 import Home from "./pages/Home";
 import Profile from "./pages/Profile";
 import Dashboard from "./pages/Dashboard";
 
-export { Button, UploadPage, Home, Profile, Dashboard };
+export {  UploadPage, Home, Profile, Dashboard };
